bf2fe195d79e38c4eedf43f54f9a7f5c7cafa5b6
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-12-19 23:25:50 +0600

    use tamp variable for string

--- Interview realated question/index2.js
+++ Interview realated question/index2.js
@@ -1,7 +1,14 @@
 // Swaping values
-const array = [1,2,3,4,5]
+const array = [1, 2, 3, 4, 5];
 // use tamp variable
-let temp = array[0]
-array[0]=array[4]
-array[4]=temp
+let temp = array[0];
+array[0] = array[4];
+array[4] = temp;
 console.log(array);
+
+// use tamp variable for string
+const rakib = ['md', 'abdur', 'Razzak'];
+const temp1 = rakib[0];
+rakib[0] = rakib[2];
+rakib[2] = temp1;
+console.log(rakib);
