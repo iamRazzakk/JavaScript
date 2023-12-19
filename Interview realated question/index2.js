f3687c0f7509517efb87428776b3d27a2f996f62
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-12-19 23:57:01 +0600

    array destructuring for swaping values

--- Interview realated question/index2.js
+++ Interview realated question/index2.js
@@ -1,14 +1,25 @@
 // Swaping values
-const array = [1, 2, 3, 4, 5];
+// const array = [1, 2, 3, 4, 5];
 // use tamp variable
-let temp = array[0];
-array[0] = array[4];
-array[4] = temp;
-console.log(array);
+// let temp = array[0];
+// array[0] = array[4];
+// array[4] = temp;
+// console.log(array);
 
 // use tamp variable for string
-const rakib = ['md', 'abdur', 'Razzak'];
-const temp1 = rakib[0];
-rakib[0] = rakib[2];
-rakib[2] = temp1;
-console.log(rakib);
+// const rakib = ['md', 'abdur', 'Razzak'];
+// const temp1 = rakib[0];
+// rakib[0] = rakib[2];
+// rakib[2] = temp1;
+// console.log(rakib);
+
+
+// array destructuring
+// const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// [array1[0], array1[8]] = [array1[8], array1[0]];
+// console.log(array1);
+
+// array destructuring for string
+// const myName = ["Md", "Abdur", "Razzak", "Rakib"];
+// [myName[0], myName[3]]=[myName[3], myName[0]]
+// console.log(myName);
