85d408980c9794045620c062450b69f62690dc1b
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2024-03-21 17:23:38 +0600

    add another one find large number

--- 20 JavaScript/Index.js
+++ 20 JavaScript/Index.js
@@ -1,10 +1,10 @@
 //Write a function to reverse a given string in JavaScript.
-// const myFunction = (string) => {
-//   return string.split("").reverse().join("");
-// };
-// const stringFunction = "Md Abdur Razzak";
-// const reverseString = myFunction(stringFunction);
-// console.log(reverseString);
+const myFunction = (string) => {
+  return string.split("").reverse().join("");
+};
+const stringFunction = "Md Abdur Razzak";
+const reverseString = myFunction(stringFunction);
+console.log(reverseString);
 
 // Find The largest number of array
 const largestNumber = (arr) => {
@@ -17,3 +17,9 @@ const largestNumber = (arr) => {
   return max;
 };
 console.log(largestNumber([1, 2, 3, 4, 9, 10, 15, 1, 0]));
+
+// Another one
+const largeNumber = (arr) => {
+  return Math.max(...arr);
+};
+console.log(largeNumber([1, 2, 3, 4, 9, 10, 15, 1, 0]));
