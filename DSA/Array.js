4db3f88f62b032490aada71b856d0e8dde6f5157
iamrazzakk <rakibt23p@gmail.com>
2025-05-09 16:24:02 +0600

    refactor: update array initialization and add sum calculation example

--- DSA/Array.js
+++ DSA/Array.js
@@ -1,11 +1,23 @@
 // let arr = []
-let arr = new Array(5)
-arr.push(100)
-arr.push(90)
-console.log(arr[0]);
+// // let arr = new Array(5)
+// arr.push(100)
+// arr.push(90)
+// console.log(arr[0]);
 
-arr.pop()
+// arr.pop()
 
-arr[8] = 7
-console.log(arr);
+// arr[8] = 7
+// console.log(arr);
 
+//  what is array ?
+// Ans: Array is linear DS which stores multiple value in a continuous manner
+
+// linear means ------- one by one 
+
+
+let arr = [10, 20, 30, 40, 50, 60]
+let sum = 0
+for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i]
+}
+console.log(sum);
