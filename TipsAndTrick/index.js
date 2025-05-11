5f7516923fe026be66b5b5b7aff46ec56683d4ad
iamrazzakk <rakibt23p@gmail.com>
2025-05-11 13:05:50 +0600

    learning find false value

--- TipsAndTrick/index.js
+++ TipsAndTrick/index.js
@@ -1,5 +1,25 @@
-// remove false value from arrar
-const arr = [0, 1, false, 2, '', 3, null, 'hello', undefined, true];
-
-const truthyValue = arr.filter(Boolean)
-console.log(truthyValue)
+// Example of a complex array with various false values
+const complexArray = [
+    0,
+    1,
+    false,
+    true,
+    null,
+    undefined,
+    NaN,
+    '',
+    'Hello',
+    [],
+    [0, false, ''],
+    {},
+    { key: 'value' },
+    -1,
+    42,
+    'false',
+    '0'
+];
+// ! it's just remove false can't remove falsy value
+//     const arr = complexArray.filter(value => value !== false)
+// console.log(arr)
+    // const arr = complexArray.filter(Boolean)
+    // console.log('Original Array:', arr);
